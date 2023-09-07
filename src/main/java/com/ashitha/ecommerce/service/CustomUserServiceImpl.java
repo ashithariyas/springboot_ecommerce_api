687f3f23677136d9f1690664c560346ade1878dc
Ashitha Riyas <ashithariyas13@gmail.com>
2023-09-07 15:04:10 +0200

    1.Implemented Authentication api 2.Secured Endpoints with JWT

--- src/main/java/com/ashitha/ecommerce/service/CustomUserServiceImpl.java
+++ src/main/java/com/ashitha/ecommerce/service/CustomUserServiceImpl.java
@@ -1,5 +1,35 @@
 package com.ashitha.ecommerce.service;
 
-public class CustomUserServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ashitha.ecommerce.model.User;
+import com.ashitha.ecommerce.repository.UserRepository;
+
+@Service
+public class CustomUserServiceImpl implements UserDetailsService {
+
+	private UserRepository userRepository;
+
+	public CustomUserServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("user not found with email " + username);
+		}
+
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+	}
 
 }
